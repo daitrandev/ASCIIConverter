ba6f6bb74ee8b8634dbfe8938c63cf3b62faf191
DaiTran <tranhieudai13@gmail.com>
2020-07-05 16:13:19 +0700

    put all string resources into Resouces

--- ASCIIConverter/AppDelegate.swift
+++ ASCIIConverter/AppDelegate.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 import IQKeyboardManagerSwift
-import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
