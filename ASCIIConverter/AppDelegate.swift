26dc0c4066454008f77abc1bd56fffd0d4ba7e1c
DaiTran <tranhieudai13@gmail.com>
2020-12-22 20:36:05 +0700

    enable touch outside to dismiss

--- ASCIIConverter/AppDelegate.swift
+++ ASCIIConverter/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UIFont.loadCustomFonts()
         
         IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         
         window = UIWindow()
         window?.makeKeyAndVisible()
@@ -81,7 +82,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
